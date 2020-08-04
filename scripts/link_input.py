aa1d66b2473e8f284a099a61db91543ab269ef09
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2020-08-04 14:39:24 +0000

    Update linker to use GEFS_COM
    The rocoto workflow already has to set the location of the GEFS COM
    directory for the file dependencies of the link_input job, so instead
    of using compath.py in the link_input script itself, the script is
    updated to use GEFS_COM from the rocoto workflow. This means the
    dependency and script are always consistent with one another and makes
    it easy to change the location should a case need to be rerun from tape.

--- scripts/link_input.py
+++ scripts/link_input.py
@@ -14,18 +14,11 @@ import argparse
 print = partial(print, flush=True)
 
 cycle_frequency = 6
-max_iterations = 100
 n_members = 21
 forecast_hours = range(0, 385, 6)
 
-source_dir = subprocess.getoutput('compath.py gens/prod')
-source_pgrb2_dict = {'a': f"{source_dir}/gefs.%Y%m%d/%H/pgrb2a", 'b': f"{source_dir}/gefs.%Y%m%d/%H/pgrb2b"}
+source_pattern = '{source_dir}/gefs.%Y%m%d/%H/pgrb2{grib_type}'
 filepattern = '{directory}/ge{member}.t%Hz.pgrb2{grib_type}f{forecast_hour:02d}'
-
-
-# Stop looking backwards after this many cycles don't have data
-# This value be at least 2 to account for the current run not having completed
-max_failures = 3
 
 members = list(map(lambda m: f'c{m:02d}' if (m == 0) else f'p{m:02d}', range(0, n_members)))
 
@@ -40,9 +33,13 @@ def main():
 	if(input_dir is None):
 		input_dir = str(Path(__file__).parent.parent) + "/input"
 
+	source_dir = os.environ.get('GEFS_COM')
+	if(source_dir is None):
+		source_dir = subprocess.getoutput('compath.py gens/prod')
+
 	print(time.strftime(f'Checking %Y%m%d_%H'))
 	destination_dir = time.strftime(f'{input_dir}/%Y%m%d%H')
-	source_filelist = get_source_filenames(time)
+	source_filelist = get_source_filenames(source_dir, time)
 	link_filelist = get_link_filenames(destination_dir, time)
 
 	if all([os.path.islink(f) for f in link_filelist]):
@@ -54,6 +51,8 @@ def main():
 
 	elif any([not os.path.isfile(f) for f in source_filelist]):
 		print("  Files don't exist for this time")
+		for f2 in source_filelist:
+			print(f'{f2}: {os.path.isfile(f2)}')
 		quit(100)
 
 	else:
@@ -74,8 +73,8 @@ def get_link_filenames(directory: str, when: datetime) -> typing.List[str]:
 	return files
 
 
-def get_source_filenames(when: datetime) ->  typing.List[str]:
-	files = [when.strftime(filepattern.format(directory=when.strftime(source_pgrb2_dict[grib_type]), grib_type=grib_type, member=member, forecast_hour=forecast_hour))
+def get_source_filenames(source_dir: str, when: datetime) -> typing.List[str]:
+	files = [when.strftime(filepattern.format(directory=when.strftime(source_pattern.format(source_dir=source_dir, grib_type=grib_type)), grib_type=grib_type, member=member, forecast_hour=forecast_hour))
 		for grib_type in ['a', 'b']
 		for member in members
 		for forecast_hour in forecast_hours]
