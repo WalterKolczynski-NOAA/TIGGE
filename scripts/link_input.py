0df2ab66fe7810b8c2d4777c152ae0894091092f
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2020-09-22 02:44:47 +0000

    Update link job for GEFS v12
    The link job is updated to handle the new file pattern of GEFS v12.
    Starting with GEFS v12, the 0.5-deg files will be used instead of
    the discontinued 1-deg files.
    
    In order to maintain backwards compatibility for reprocessing older
    dates, the paths, filenames, and number of members are determined
    based on the initialization time. Currently the transition time is
    earlier than the operational switchover for testing purposes. This
    will be changed to the actual operational cycle in a later commit.
    
    Refs: #1

--- scripts/link_input.py
+++ scripts/link_input.py
@@ -14,20 +14,39 @@ import argparse
 print = partial(print, flush=True)
 
 cycle_frequency = 6
-n_members = 21
 forecast_hours = range(0, 385, 6)
 
-source_pattern = '{source_dir}/gefs.%Y%m%d/%H/pgrb2{grib_type}'
-filepattern = '{directory}/ge{member}.t%Hz.pgrb2{grib_type}f{forecast_hour:02d}'
 
-members = list(map(lambda m: f'c{m:02d}' if (m == 0) else f'p{m:02d}', range(0, n_members)))
+def get_source_path(source_dir: str, time: datetime.datetime, grib_type: str) -> str:
+	if time < datetime.datetime(2020, 9, 13, 12):
+		return time.strftime(f'{source_dir}/gefs.%Y%m%d/%H/pgrb2{grib_type}')
+	else:
+		return time.strftime(f'{source_dir}/gefs.%Y%m%d/%H/atmos/pgrb2{grib_type}p5')
+
+
+def get_filename(directory: str, time: datetime.datetime, member: int, forecast_hour: int, grib_type: str) -> str:
+	if time < datetime.datetime(2020, 9, 13, 12):
+		return time.strftime(f'{directory}/ge{member}.t%Hz.pgrb2{grib_type}f{forecast_hour:02d}')
+	else:
+		return time.strftime(f'{directory}/ge{member}.t%Hz.pgrb2{grib_type}.0p50.f{forecast_hour:03d}')
+
+
+def get_members(time: datetime.datetime) -> typing.List[str]:
+	if time < datetime.datetime(2020, 9, 13, 12):
+		n_members = 21
+	else:
+		n_members = 31
+
+	return list(map(lambda m: f'c{m:02d}' if (m == 0) else f'p{m:02d}', range(0, n_members)))
 
 
 def main():
-	parser = argparse.ArgumentParser(description="Creates symbolic links to 1.0 deg GEFS products for TIGGE")
+	parser = argparse.ArgumentParser(description="Creates symbolic links to  GEFS products for TIGGE")
 	parser.add_argument('time', type=lambda s: datetime.datetime.strptime(s, "%Y%m%d%H"), help="Cycle date in YYYYMMDDHH format")
 
 	time = parser.parse_args().time
+
+	members = get_members(time=time)
 
 	input_dir = os.environ.get('TIGGE_INPUT')
 	if(input_dir is None):
@@ -39,8 +58,8 @@ def main():
 
 	print(time.strftime(f'Checking %Y%m%d_%H'))
 	destination_dir = time.strftime(f'{input_dir}/%Y%m%d%H')
-	source_filelist = get_source_filenames(source_dir, time)
-	link_filelist = get_link_filenames(destination_dir, time)
+	source_filelist = get_source_filenames(source_dir, time, members)
+	link_filelist = get_link_filenames(destination_dir, time, members)
 
 	if all([os.path.islink(f) for f in link_filelist]):
 		if all([os.path.isfile(f) for f in link_filelist]):
@@ -65,16 +84,16 @@ def main():
 	exit(0)
 
 
-def get_link_filenames(directory: str, when: datetime) -> typing.List[str]:
-	files = [when.strftime(filepattern.format(directory=directory, grib_type=grib_type, member=member, forecast_hour=forecast_hour))
+def get_link_filenames(directory: str, when: datetime, members: typing.List[str]) -> typing.List[str]:
+	files = [get_filename(directory=directory, time=when, member=member, forecast_hour=forecast_hour, grib_type=grib_type)
 		for grib_type in ['a', 'b']
 		for member in members
 		for forecast_hour in forecast_hours]
 	return files
 
 
-def get_source_filenames(source_dir: str, when: datetime) -> typing.List[str]:
-	files = [when.strftime(filepattern.format(directory=when.strftime(source_pattern.format(source_dir=source_dir, grib_type=grib_type)), grib_type=grib_type, member=member, forecast_hour=forecast_hour))
+def get_source_filenames(source_dir: str, when: datetime, members: typing.List[str]) -> typing.List[str]:
+	files = [get_filename(directory=get_source_path(source_dir, time=when, grib_type=grib_type), time=when, member=member, forecast_hour=forecast_hour, grib_type=grib_type)
 		for grib_type in ['a', 'b']
 		for member in members
 		for forecast_hour in forecast_hours]
