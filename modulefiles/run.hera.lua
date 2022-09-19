a050b07a3ecc6a3b44b1fdcd842a3fde75a246e0
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2022-09-19 02:24:21 +0000

    Updates to run on Hera

--- modulefiles/run.hera.lua
+++ modulefiles/run.hera.lua
@@ -1,2 +1,6 @@
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+
+load("hpc/1.1.0")
+load("hpc-intel/18.0.5.274")
 load("intelpython/3.6.8")
 load("hpss/hpss")
