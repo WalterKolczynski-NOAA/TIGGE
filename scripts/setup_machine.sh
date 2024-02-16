27acbfec66456ee0475b619a1905c0560b4f5d2e
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2024-02-16 00:08:46 +0000

    Reset modules at start

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -2,10 +2,15 @@
 
 set +x
 if [[ -d /scratch1 ]]; then
-	source /apps/lmod/8.5.2/init/sh > /dev/null 2>&1
+	if ( ! eval module help > /dev/null 2>&1 ); then
+		source /apps/lmod/lmod/init/bash
+	fi
+	export LMOD_SYSTEM_DEFAULT_MODULES='contrib'
+	module reset
 	export MACHINE="hera"
 
 elif [[ -d /lfs/h2 ]]; then
+	module reset
 	export MACHINE="wcoss2"
 fi
 ${TRACE_ON:-set -x}
