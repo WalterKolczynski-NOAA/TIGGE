c33fa57f1ae65fccf623445de2bce1ad873f5e06
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2025-01-24 20:45:10 +0000

    Calculate sha256 hash for final tarball
    Caculates a sha256 hash and saves it to a file as a data integrity
    check for the tarfile.

--- scripts/transfer_output.sh
+++ scripts/transfer_output.sh
@@ -8,21 +8,22 @@ time=${1}
 module use ../modulefiles > /dev/null 2>&1
 module load run.${MACHINE} > /dev/null 2>&1
 
-year=$(echo $time | cut -c1-4)
-month=$(echo $time | cut -c5-6)
+year=${time:1:4}
+month=${time:4:2}
 
 cd "${TIGGE_OUTPUT}/archive"
 tarfile="tigge-kwbc-${time}.tar"
-remote="rzdm:/home/ftp/emc/ufs/global/ensemble/tigge/$tarfile"
+remote="rzdm:/home/ftp/emc/ufs/global/ensemble/tigge"
 
-rsync -a -P -e 'ssh -C -F /home/Walter.Kolczynski/ssh_config' --partial-dir=.tmp $tarfile $remote
+sha256sum "${tarfile}" > "${tarfile}.sha256"
+rsync -a -P -e 'ssh -C -F /home/Walter.Kolczynski/ssh_config' --partial-dir=.tmp "${tarfile}" "${tarfile}.sha256" "${remote}/"
 
 err=$?
-if (( err != 0 )); then
-	echo "FATAL ERROR from rsync: $err"
-	exit $err
+if ((err != 0)); then
+	echo "FATAL ERROR from rsync: ${err}"
+	exit "${err}"
 else
 	echo "rsync to rzdm completed successfully"
 fi
 
-exit $err
+exit "${err}"
