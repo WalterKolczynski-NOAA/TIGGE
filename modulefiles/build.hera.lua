1dc3689dfa12ee6818986e004fbb7135ca640914
Eric.Sinsky <eric.sinsky@noaa.gov>
2025-07-28 12:25:32 +0000

    Add openjpeg module for Ursa build

--- modulefiles/build.hera.lua
+++ modulefiles/build.hera.lua
@@ -7,6 +7,7 @@ prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneap
 load("stack-oneapi/2024.2.1")
 load("intel-oneapi-compilers/2023.2.0")
 load("jasper/2.0.32")
+load("openjpeg/2.3.1")
 load("libpng/1.6.37")
 load("cmake/3.30.2")
 
